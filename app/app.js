b2734440f7b90166ef44f7981fd15bcac09ccf6a
Sayam Gautam <sayamgautam1@gmail.com>
2022-04-07 16:37:15 +0930

    add basic functionality of display clear, add ,subtract and multiply

--- app/app.js
+++ app/app.js
@@ -1,8 +1,63 @@
 let displayScreenEl = document.getElementById("screen");
 let buttonEl = document.querySelectorAll("button");
+let numberEl = document.querySelectorAll(".number");
+let operationEl = document.querySelectorAll(".operation");
+let resultEl = document.querySelector("#result");
+let clearAllEl = document.getElementById("clear");
+let displayValues = [];
+
 buttonEl.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    displayScreenEl.innerText = btn.textContent;
-    console.log(btn.textContent);
+  btn.addEventListener("click", function (e) {
+    if (numberEl) {
+      console.log(btn.innerText);
+    } else if (clearAllEl) {
+      clearContent();
+      console.log(btn.innerText);
+    } else if (operationEl) {
+      console.log(btn.innerText);
+    } else {
+      console.log("helloworld");
+    }
   });
 });
+
+// operation function of the calculator
+let num1 = 0;
+let num2 = 0;
+let operation = "";
+
+function addition(x, y) {
+  return x + y;
+}
+function subtraction(x, y) {
+  return x - y;
+}
+function multuply(x, y) {
+  return x * y;
+}
+function division(x, y) {
+  return x / y;
+}
+function percentage(x, y) {
+  return x % y;
+}
+function operate(currentOperator, a, b) {
+  switch (currentOperator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "÷":
+      return divide(a, b);
+    case "%":
+      return percentage(a, b);
+    default:
+      null;
+  }
+}
+function clearContent() {
+  displayScreenEl.textContent = "0";
+  displayValues = [];
+}
